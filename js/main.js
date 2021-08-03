056d3b01733cdf024a88fdb03ca4592712ba23c1
firassemgey <firas.semgey@gmail.com>
2021-08-03 20:29:51 +0200

    aa

--- js/main.js
+++ js/main.js
@@ -23,6 +23,41 @@
         if ($(this).is(":checked")) {
             $('.input3-select').slideUp(300);
             $('.slide').slideDown(300);
+
+
+            $('.validate-form').on('submit',function(){
+
+                var check = true;
+
+                if($(Restaurant).val().trim() == ''){
+                    showValidate(Restaurant);
+                    check=false;
+                }
+
+
+                if($(name).val().trim() == ''){
+                    showValidate(name);
+                    check=false;
+                }
+
+
+                if($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) !== null) {
+                    showValidate(email);
+                    check=false;
+                }
+                if ($(phone).val().trim() == '' && $(phone).length < 10){
+                    showValidate(phone);
+                    check=false;
+                }
+
+                if($(message).val().trim() == ''){
+                    showValidate(message);
+                    check=false;
+                }
+
+                return check;
+            });
+
         }
     });
 
@@ -30,6 +65,29 @@
         if ($(this).is(":checked")) {
             $('.input3-select').slideDown(300);
             $('.slide').slideUp(300);
+
+
+            $('.validate-form').on('submit',function(){
+
+                var check = true;
+
+
+
+
+                if($(name).val().trim() == ''){
+                    showValidate(name);
+                    check=false;
+                }
+
+                if ($(phone).val().trim() == '' && $(phone).length < 10){
+                    showValidate(phone);
+                    check=false;
+                }
+
+
+                return check;
+            });
+
         }
     });
         
@@ -37,32 +95,12 @@
     
     /*==================================================================
     [ Validate ]*/
+    var Restaurant = $('.validate-input input[name="Restaurant"]');
     var name = $('.validate-input input[name="name"]');
     var email = $('.validate-input input[name="email"]');
+    var phone = $('.validate-input input[name="phone"]');
     var message = $('.validate-input textarea[name="message"]');
 
-
-    $('.validate-form').on('submit',function(){
-        var check = true;
-
-        if($(name).val().trim() == ''){
-            showValidate(name);
-            check=false;
-        }
-
-
-        if($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-            showValidate(email);
-            check=false;
-        }
-
-        if($(message).val().trim() == ''){
-            showValidate(message);
-            check=false;
-        }
-
-        return check;
-    });
 
 
     $('.validate-form .input3').each(function(){
@@ -82,7 +120,7 @@
 
         $(thisAlert).removeClass('alert-validate');
     }
-    
+
     
 
 })(jQuery);
